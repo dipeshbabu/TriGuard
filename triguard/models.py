f830bcf929a7d04d4b1f520f1a112dd2913752af
Dipesh Babu <dm6259@cs-01kmkpqz521qg00jhc473gzez4.us-central1-c.c.lightning-ai-prod.internal>
2026-03-26 00:41:29 +0000

    handle non-contiguous tesnsors

--- triguard/models.py
+++ triguard/models.py
@@ -30,7 +30,7 @@ class SimpleCNN(nn.Module):
     def forward(self, x):
         x = self.pool(F.relu(self.conv1(x)))
         x = self.pool(F.relu(self.conv2(x)))
-        x = x.view(x.size(0), -1)
+        x = x.reshape(x.size(0), -1)
         x = F.relu(self.fc1(x))
         x = self.dropout(x)
         x = self.fc2(x)
